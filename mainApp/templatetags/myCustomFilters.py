938ea92c8470491ba92ed07b687c79770d20ccdc
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-10-18 15:59:03 +0530

    customfilter

--- mainApp/templatetags/myCustomFilters.py
+++ mainApp/templatetags/myCustomFilters.py
@@ -10,3 +10,25 @@ def paymentMode(Request, num):
     else:
         return "NetBanking"
 
+@register.filter(name="paymentStatus")
+def paymentStatus(Request, num):
+    if(num==0):
+        return "Pending"
+    else:
+        return "Done"
+    
+
+@register.filter(name="orderStatus")
+def orderStatus(Request, num):
+    if(num==0):
+        return "Order is Placed"
+    elif(num==1):
+        return "Order is Packed"
+    elif(num==2):
+        return "Ready to Dispatch"
+    elif(num==3):
+        return "Dispatched"
+    elif(num==4):
+        return "Out For Delivery"
+    else:
+        return "Delivered"
